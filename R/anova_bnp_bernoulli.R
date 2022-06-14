95ad636140ebac00815186344e42a93b5dd03ca9
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-06-14 03:46:30 +0000

    Use the paper's notations

--- R/anova_bnp_bernoulli.R
+++ R/anova_bnp_bernoulli.R
@@ -6,22 +6,24 @@
 #' @param warmup the number of warmup mcmc iterations (default 2000).
 #' @param seed the seed for random number generation (default 1L).
 #' @param rho the hyperparameter \eqn{\rho} (default 1.0).
-#' @param a0 the hyperparameter \eqn{a_0} (default 1.0).
-#' @param b0 the hyperparameter \eqn{b_0} (default 1.0).
-#' @param a1 the hyperparameter \eqn{a_1} (default 2.0).
-#' @param b1 the hyperparameter \eqn{b_1} (default 4.0).
+#' @param a the hyperparameter \eqn{a} (default 1.0).
+#' @param b the hyperparameter \eqn{b} (default 1.0).
+#' @param a2 the hyperparameter \eqn{a_2} (default 2.0).
+#' @param b2 the hyperparameter \eqn{b_2} (default 4.0).
 #' @return An object of class 'anova_bnp_model'.
 #' @importFrom dplyr as_tibble
 #' @importFrom JuliaConnectoR juliaImport
 #' @export
 anova_bnp_bernoulli <- function(
   y, X, iter = 4000L, warmup = 2000L, seed = 1L, rho = 1.0,
-  a0 = 1.0, b0 = 1.0, a1 = 2.0, b1 = 4.0, lb = min(y), ub = max(y)
+  a = 1.0, b = 1.0, a2 = 2.0, b2 = 4.0, 
+  lb = min(y), ub = max(y)
 ) {
   ANOVADDPTest <- juliaImport("ANOVADDPTest")
   fit <- ANOVADDPTest$anova_bnp_bernoulli(
     y, X, iter = iter, warmup = warmup, seed = seed, rho = rho,
-    a0 = a0, b0 = b0, a1 = a1, b1 = b1, lb = lb, ub = ub
+    a = a, b = b, a2 = a2, b2 = b2, 
+    lb = lb, ub = ub
   )
   out <-
     list(
--- R/anova_bnp_normal.R
+++ R/anova_bnp_normal.R
@@ -1,33 +1,36 @@
 #' Fit a Bayesian nonparametric factorial ANOVA model with Gaussian kernel.
 #'
-#' @param y A continuous response vector.
-#' @param X A design matrix (full of integer covariates).
+#' @param y a continuous response vector.
+#' @param X a design matrix (full of integer covariates).
 #' @param iter the total number of mcmc iterations (default 4000).
 #' @param warmup the number of warmup mcmc iterations (default 2000).
 #' @param seed the seed for random number generation (default 1L).
 #' @param n the number of points y0 for computing p(y0 | y) (default 50L).
-#' The final grid is conformed by n equispaced point from
-#' \code{min(y) - sd(y) / 2} to \code{max(y) -  sd(y) / 2}
+#' The final grid is conformed by n equispaced points from `lb` to `ub`, see
+#' the arguments `lb` and `ub`  
 #' @param rho the hyperparameter \eqn{\rho} (default 1.0).
-#' @param a0 the hyperparameter \eqn{a_0} (default 1.0).
+#' @param a the hyperparameter \eqn{a} (default 1.0).
+#' @param b the hyperparameter \eqn{b} (default 1.0).
+#' @param mu0 the hyperparameter \eqn{\mu_0} (default 0.0).
+#' @param lambda0 the hyperparameter \eqn{\lambda_0} (default 1.0).
+#' @param a0 the hyperparameter \eqn{a_0} (default 2.0).
 #' @param b0 the hyperparameter \eqn{b_0} (default 1.0).
-#' @param v0 the hyperparameter \eqn{v_0} (default 2.0).
-#' @param r0 the hyperparameter \eqn{r_0} (default 1.0).
-#' @param u0 the hyperparameter \eqn{u_0} (default 0.0).
-#' @param s0 the hyperparameter \eqn{s_0} (default 1.0).
+#' @param lb the lower bound of the prediction grid, default: `mean(y) - 3.0 * sd(y)`
+#' @param ub the upper bound of the prediction grid, default: `mean(y) + 3.0 * sd(y)`
 #' @return An object of class 'anova_bnp_model'.
 #' @importFrom dplyr as_tibble
 #' @importFrom JuliaConnectoR juliaImport
 #' @export
 anova_bnp_normal <- function(
   y, X, iter = 4000L, warmup = 2000L, seed = 1L, n = 50L, rho = 1.0,
-  a0 = 1.0, b0 = 1.0, v0 = 2.0, r0 = 1.0, u0 = 0.0, s0 = 1.0,
-  lb = mean(y) - 0.5 * sd(y), ub = mean(y) + 0.5 * sd(y)
+  a = 1.0, b = 1.0, a0 = 2.0, lambda0 = 1.0, mu0 = 0.0, b0 = 1.0,
+  lb = mean(y) - 3.0 * sd(y), ub = mean(y) + 3.0 * sd(y)
 ) {
   ANOVADDPTest <- juliaImport("ANOVADDPTest")
   fit <- ANOVADDPTest$anova_bnp_normal(
     y, X, iter = iter, warmup = warmup, seed = seed, n = n, rho = rho,
-    a0 = a0, b0 = b0, v0 = v0, r0 = r0, u0 = u0, s0 = s0, lb = lb, ub = ub
+    a = a, b = b, mu0 = mu0, lambda0 = lambda0, a0 = a0, b0 = b0, 
+    lb = lb, ub = ub
   )
   out <-
     list(
