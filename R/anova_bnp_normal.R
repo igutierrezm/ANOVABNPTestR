5c461e5bbd032cbf1aebf29b1423f63cad0194bd
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-06-14 23:46:44 +0000

    Add option `standardize_y`

--- R/anova_bnp_normal.R
+++ R/anova_bnp_normal.R
@@ -7,7 +7,7 @@
 #' @param seed the seed for random number generation (default 1L).
 #' @param n the number of points y0 for computing p(y0 | y) (default 50L).
 #' The final grid is conformed by n equispaced points from `lb` to `ub`, see
-#' the arguments `lb` and `ub`  
+#' the arguments `lb` and `ub`
 #' @param rho the hyperparameter \eqn{\rho} (default 1.0).
 #' @param a the hyperparameter \eqn{a} (default 1.0).
 #' @param b the hyperparameter \eqn{b} (default 1.0).
@@ -17,6 +17,8 @@
 #' @param b0 the hyperparameter \eqn{b_0} (default 1.0).
 #' @param lb the lower bound of the prediction grid, default: `mean(y) - 3.0 * sd(y)`
 #' @param ub the upper bound of the prediction grid, default: `mean(y) + 3.0 * sd(y)`
+#' @param standardize_y Select `TRUE` to internally standardize the data,
+#' fit the model, and present the results in the original scale
 #' @return An object of class 'anova_bnp_model'.
 #' @importFrom dplyr as_tibble
 #' @importFrom JuliaConnectoR juliaImport
@@ -24,13 +26,14 @@
 anova_bnp_normal <- function(
   y, X, iter = 4000L, warmup = 2000L, seed = 1L, n = 50L, rho = 1.0,
   a = 1.0, b = 1.0, a0 = 2.0, lambda0 = 1.0, mu0 = 0.0, b0 = 1.0,
-  lb = mean(y) - 3.0 * sd(y), ub = mean(y) + 3.0 * sd(y)
+  lb = mean(y) - 3.0 * sd(y), ub = mean(y) + 3.0 * sd(y),
+  standardize_y = FALSE
 ) {
   ANOVADDPTest <- juliaImport("ANOVADDPTest")
   fit <- ANOVADDPTest$anova_bnp_normal(
     y, X, iter = iter, warmup = warmup, seed = seed, n = n, rho = rho,
-    a = a, b = b, mu0 = mu0, lambda0 = lambda0, a0 = a0, b0 = b0, 
-    lb = lb, ub = ub
+    a = a, b = b, mu0 = mu0, lambda0 = lambda0, a0 = a0, b0 = b0,
+    lb = lb, ub = ub, standardize_y = standardize_y
   )
   out <-
     list(
