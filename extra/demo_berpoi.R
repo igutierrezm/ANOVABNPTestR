8621d2ad714b72b5f8239a090efba8987d5120e8
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-11-03 01:51:24 +0000

    Improve the demo for the berpoi

--- extra/demo_berpoi.R
+++ extra/demo_berpoi.R
@@ -1,9 +1,33 @@
 library(ANOVABNPTestR)
+library(dplyr)
+library(tidyr)
+ANOVABNPTestR::setup()
 
-N <- 1000
-x <- as.integer(1 + (runif(2 * N) < 0.5))
-Xmat <- matrix(x, nrow = N, ncol = 2)
-yvec <- rpois(N, 1) + (Xmat[, 1] == 2) * (Xmat[, 2] == 2)
+dberpoi <- function(x, lambda, alpha) {
+  alpha * dpois(x - 1, lambda) + (1 - alpha) * dpois(x, lambda)
+}
+
+# Simulate a sample
+N <- 2000
+lambda <- 1
+alpha <- 0.5
+Xmat <- matrix(1L, nrow = N, ncol = 1)
+yvec <- rpois(N, lambda) + rbinom(N, 1, alpha)
+
+# Fit the model
 fit <- anova_bnp_berpoi(yvec, Xmat)
-hypothesis_post_simple(fit)
-hypothesis_post_interaction(fit)
+
+# Extract the posterior predictive density
+df <- f_post(fit)
+
+# Add the true density
+df <-
+  df |>
+  dplyr::mutate(ftrue = dberpoi(y, lambda, alpha)) |>
+  dplyr::rename(fbnp = f) |>
+  tidyr::pivot_longer(cols = c(ftrue, fbnp))
+
+# Plot the true and fitted densities
+df |>
+  ggplot2::ggplot(ggplot2::aes(x = y, color = name, y = value)) +
+  ggplot2::geom_line()
